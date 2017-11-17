ecb07c302abb030f957d8670d61b2ce4856c988a
Tan Ching Yang <simon.tan15@yahoo.com>
2017-11-17 21:57:16 +0800

    Add varification on background.js

--- WebsitetoPdf v1.3/background.js
+++ WebsitetoPdf v1.3/background.js
@@ -8,14 +8,20 @@ var apiVersionUrl = pdfCrowdUrl + '/session/api-version/';
 
 function onDataReady(XMLrequest, callbacks) {
     return function(data) {
-
+        if (XMLrequest.readyState == 4) {
             if (XMLrequest.status == 200) {
+                if (callbacks.onSuccess) {
                         var data = JSON.parse(XMLrequest.responseText);
                         callbacks.onSuccess(data);
-
-            } 
+                        //window.alert("success");
+                }
+            } else {
+                if (callbacks.onError)
+                    callbacks.onError(XMLrequest.responseText)
+            }
             if (callbacks.onComplete)
                 callbacks.onComplete();
+        }
     };
 }
 
@@ -27,10 +33,16 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     XMLrequest.onreadystatechange = onDataReady(XMLrequest, {
         onSuccess: function(data) {
             var apiUrl = PDFapiUrl[data.api_version];
-
+            chrome.windows.create({
+                url: "https://www.google.com/search?q=success",
+                type: "popup",
+                width: 1000,
+                height: 500
+            });
+            if (apiUrl === undefined) {
                 apiUrl = PDFapiUrl[2];
 
-
+            }
         },
         onError: function(responseText) {
             showError("Fail connect to Pdfcrowd");
